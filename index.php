c9777f4e16555d5ca35321f629672fe517bb8aef
Sean Vreeland <seanvree@hotmail.com>
2017-07-29 13:53:43 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,17 @@
 <!DOCTYPE html>
 <html>
-
-<head>
-<title>CHANGEME</title>
-
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"/>
-<style type="text/css">
-.auto-style1 {
-	color: #3F51B5;
-	font-family: "Segoe UI";
-}
-</style>
-</head>
-
+<title>Logarr</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<body>
 <body style="color: #FFFFFF; background-color: #252525">
-
 <div class="w3-container w3-center">
-  <h3 class="auto-style1"><strong>MP4 Converter:</strong></h3>
+  <h3>MP4 Converter:</h3>
 </div>
 
-<div class="w3-container w3-border w3-margin" style="background-color:#404040; word-wrap: break-word; width:auto; height:200px; overflow-y: scroll;">
+<div class="w3-container w3-border w3-margin" style="word-wrap: break-word; width:auto; height:200px; overflow-y: scroll;">
   <p> <?php
-            $filename     = "C:\sickbeard_mp4_automator\info.log";
+            $filename     = "/home/plex/bin/sickbeard_mp4_automator/info.log";
             $file_ptr     = fopen ( $filename, "r" );
             $file_size    = filesize ( $filename );
             $text         = fread ( $file_ptr, $file_size );
@@ -29,13 +19,12 @@
             echo nl2br ( $text );
         ?></p>
 </div>
-
 <div class="w3-container w3-center">
-  <h3><span class="w3-text-indigo"><strong>Headphones</strong>:</span></h3>
+  <h3>Headphones:</h3>
 </div>
-<div class="w3-container w3-border w3-margin" style="background-color:#404040; word-wrap: break-word; width:auto; height:200px; overflow-y: scroll;">
+<div class="w3-container w3-border w3-margin" style="word-wrap: break-word; width:auto; height:200px; overflow-y: scroll;">
   <p><?php
-            $filename     = "C:\headphones\logs\headphones.log";
+            $filename     = "/opt/headphones/logs/headphones.log";
             $file_ptr     = fopen ( $filename, "r" );
             $file_size    = filesize ( $filename );
             $text         = fread ( $file_ptr, $file_size );
@@ -43,13 +32,12 @@
             echo nl2br ( $text );
         ?></p>
 </div>
-
 <div class="w3-container w3-center">
-  <h3><span class="w3-text-indigo"><strong>Sonarr:</strong></span></h3>
+  <h3>Sonarr:</h3>
 </div>
-<div class="w3-container w3-border w3-margin" style="background-color:#404040; word-wrap: break-word; width:auto; height:200px; overflow-y: scroll;">
+<div class="w3-container w3-border w3-margin" style="word-wrap: break-word; width:auto; height:200px; overflow-y: scroll;">
   <p><?php
-            $filename     = 'C:\ProgramData\NzbDrone\logs\sonarr.txt';
+            $filename     = "/home/plex/.config/NzbDrone/logs/sonarr.txt";
             $file_ptr     = fopen ( $filename, "r" );
             $file_size    = filesize ( $filename );
             $text         = fread ( $file_ptr, $file_size );
@@ -57,13 +45,12 @@
             echo nl2br ( $text );
         ?></p>
 </div>
-
 <div class="w3-container w3-center">
-  <h3><span class="w3-text-indigo"><strong>Radarr:</strong></span></h3>
+  <h3>Radarr:</h3>
 </div>
-<div class="w3-container w3-border w3-margin" style="background-color:#404040; word-wrap: break-word; width:auto; height:200px; overflow-y: scroll;">
+<div class="w3-container w3-border w3-margin" style="word-wrap: break-word; width:auto; height:200px; overflow-y: scroll;">
   <p><?php
-            $filename     = 'C:\logs\radarr\radarr.txt';
+            $filename     = '/home/plex/.config/Radarr/logs/radarr.txt';
             $file_ptr     = fopen ( $filename, "r" );
             $file_size    = filesize ( $filename );
             $text         = fread ( $file_ptr, $file_size );
@@ -71,13 +58,12 @@
             echo nl2br ( $text );
         ?></p>
 </div>
-
 <div class="w3-container w3-center">
-  <h3><span class="w3-text-indigo"><strong>SABNzbd:</strong></span></h3>
+  <h3>NZBGET:</h3>
 </div>
-<div class="w3-container w3-border w3-margin" style="background-color:#404040; word-wrap: break-word; width:auto; height:200px; overflow-y: scroll;">
+<div class="w3-container w3-border w3-margin" style="word-wrap: break-word; width:auto; height:200px; overflow-y: scroll;">
   <p><?php
-            $filename     = "C:\sabnzbd\logs\sabnzbd.log";
+            $filename     = "/mnt/hdd/downloads/nzbget.log";
             $file_ptr     = fopen ( $filename, "r" );
             $file_size    = filesize ( $filename );
             $text         = fread ( $file_ptr, $file_size );
@@ -85,7 +71,5 @@
             echo nl2br ( $text );
         ?></p>
 </div>
-
 </body>
-
 </html>
