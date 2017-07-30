0b2ec06f67cfc47d737fd5c179b7f59810933a4c
Sean Vreeland <seanvree@hotmail.com>
2017-07-30 02:19:46 -0700

    Scrollbar styles

--- index.php
+++ index.php
@@ -23,6 +23,24 @@ $logs = array(
 			color: #3F51B5;
 			font-family: "Segoe UI";
 		}
+			
+			body::-webkit-scrollbar {
+				width: 10px;
+				background-color: white;
+			}
+ 
+			body::-webkit-scrollbar-track {
+				-webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
+				border-radius: 10px;
+				background-color: #252525;
+			}
+ 
+			body::-webkit-scrollbar-thumb {
+				border-radius: 10px;
+				-webkit-box-shadow: inset 0 0 6px rgba(0,0,0,.3);
+				background-color: #8E8B8B;
+			}
+			
 		    #slide {
 			border: 1px solid black;
 		}
