798ba39fee7b03e0340d1d8d648ed1b3b824c000
Sean Vreeland <seanvree@hotmail.com>
2017-08-02 04:35:55 -0700

    support files for Server DTG, Search

---
+++ serverdate.js
@@ -0,0 +1,29 @@
+var xmlHttp;
+function srvTime(){
+	try {
+		//FF, Opera, Safari, Chrome
+		xmlHttp = new XMLHttpRequest();
+	}
+	catch (err1) {
+		//IE
+		try {
+			xmlHttp = new ActiveXObject('Msxml2.XMLHTTP');
+		}
+		catch (err2) {
+			try {
+				xmlHttp = new ActiveXObject('Microsoft.XMLHTTP');
+			}
+			catch (eerr3) {
+				//AJAX not supported, use CPU time.
+				alert("AJAX not supported");
+			}
+		}
+	}
+	xmlHttp.open('HEAD',window.location.href.toString(),false);
+	xmlHttp.setRequestHeader("Content-Type", "text/html");
+	xmlHttp.send('');
+	return xmlHttp.getResponseHeader("Date");
+}
+
+var st = srvTime();
+var date = new Date(st);
