eb2b754eb29260d3e57b54ef1bc785602601eac7
jonfinley <jon.finley@me.com>
2017-08-14 11:31:45 -0400

    test

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ $logs = array(
     <head>
         <meta charset="utf-8" />
         
-        <title>YourLogs</title>
+        <title>FinFlix Logs</title>
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"/>
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	    <meta name="description" content="Demo of Real-Time Search in JavaScript" />
