e06942eb7c54a788809aee311ea134278cf33a4b
jonfinley <accounts@jonathanfinley.net>
2017-08-15 00:51:18 -0400

    Code update and Favicon

--- config.php
+++ config.php
@@ -4,10 +4,13 @@ $logs = array(
 //Ensure correct permissions are set on the target log file
 //If this page is exposed ot your WAN, check the logging applications' settings for senstive data within logs. 
 // EXAMPLE
-// "NameOfLog" =>'C:\link\to\log\file'
-"NZBtoMedia" => 'C:\logs\nzbtomedia\logs\nzbtomedia.log',
-"MP4 Converter" => 'C:\sickbeard_mp4_automator\info.log',
-"Radarr" => 'C:\ProgramData\Radarr\logs\radarr.txt',
+// "NameOfLog" =>'/link/to/log/file'
+"Sonarr" => '/home/plex/.config/NzbDrone/logs/sonarr.txt',
+"Radarr" => '/home/plex/.config/Radarr/logs/radarr.txt',
+"Headphones" => "/home/plex/logs/headphones/headphones.log",
+/*"NZBGet" => "/home/plex/logs/nzbget/nzbget.log",*/
+"MP4 Converter" => "/home/plex/bin/sickbeard_mp4_automator/info.log",
+"NZBHydra" => "/home/plex/logs/nzbhydra/nzbhydra.log",
 
 // ** Add Logs ABOVE this line **
 );
