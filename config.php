27d66b7282957a066e601565dff591fe392170c9
jonfinley <accounts@jonathanfinley.net>
2017-08-14 23:33:24 -0400

    External Config.php

--- config.php
+++ config.php
@@ -3,14 +3,11 @@ $logs = array(
 // ** Add Logs BELOW this paragraph, under the term "array" **
 //Ensure correct permissions are set on the target log file
 //If this page is exposed ot your WAN, check the logging applications' settings for senstive data within logs. 
-//I have all-most of my logs symlinked to my main user dir ~/logs/appname/ - jonfinley
-
-"Sonarr" => '/home/plex/.config/NzbDrone/logs/sonarr.txt',
-"Radarr" => '/home/plex/.config/Radarr/logs/radarr.txt',
-"Headphones" => "/home/plex/logs/headphones/headphones.log",
-/*"NZBGet" => "/home/plex/logs/nzbget/nzbget.log",*/
-"MP4 Converter" => "/home/plex/bin/sickbeard_mp4_automator/info.log",
-"NZBHydra" => "/home/plex/logs/nzbhydra/nzbhydra.log",
+// EXAMPLE
+// "NameOfLog" =>'C:\link\to\log\file'
+"NZBtoMedia" => 'C:\logs\nzbtomedia\logs\nzbtomedia.log',
+"MP4 Converter" => 'C:\sickbeard_mp4_automator\info.log',
+"Radarr" => 'C:\ProgramData\Radarr\logs\radarr.txt',
 
 // ** Add Logs ABOVE this line **
 );
