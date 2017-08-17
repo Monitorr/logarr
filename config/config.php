34a6173bb8eac20a247385b53d14b77986d124bd
Sean Vreeland <seanvree@hotmail.com>
2017-08-17 06:56:40 -0700

    NOTE: log paths being case sensitive for WIN

--- config/config.php
+++ config/config.php
@@ -1,10 +1,16 @@
 <?php // config style adapted from @causeFX and this website: https://www.abeautifulsite.net/a-better-way-to-write-config-files-in-php 
 $logs = array(
-// ** Add Logs BELOW this paragraph, under the term "array" **
-//Ensure correct permissions are set on the target log file
-//If this page is exposed ot your WAN, check the logging applications' settings for senstive data within logs. 
+
+// ** Log paths are CASE SESITIVE in a windows enviroment **
+// Ensure correct permissions are set on the target log file
+// Ensure the logging applications' settings are set to "roll over/refresh" the log files at regular intervals
+// Depening on your enviroment, large log files could cause your webserver to crash.
+// Recomended individual log files be NO MORE than ~2MB in size  
+// If this page is exposed to your WAN, check the logging applications' settings for senstive data within logs
+// ** Add Logs BELOW paragraph **
 // EXAMPLE
 // "NameOfLog" =>'C:/link/to/log/file'
+    
 "NZBtoMedia" => 'C:\logs\nzbtomedia\logs\nzbtomedia.log',
 "MP4 Converter" => 'C:\sickbeard_mp4_automator\info.log',
 "Radarr" => 'C:\ProgramData\Radarr\logs\radarr.txt',
