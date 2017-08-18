422acb608e482655dae2d76f93b1f19ddb239a77
Sean Vreeland <seanvree@hotmail.com>
2017-08-18 14:43:25 -0700

    Fixed Search Functions

--- index.php
+++ index.php
@@ -44,7 +44,11 @@ include 'config/config.php';
         <meta name="robots" content="NOINDEX, NOFOLLOW">
         <meta name="viewport" content="width=device-width,initial-scale=1" />
 
+        <script src="jquery.min.js"></script>
+              
         <script src="pace.js"></script>
+
+        <script src="jquery_search.js"></script>
 
         <script type="text/javascript" src="hilitor.js"></script>
         <script type="text/javascript">
@@ -74,8 +78,8 @@ include 'config/config.php';
 
     </head>
     
-    <body style="border: 10px solid #252525; color: #FFFFFF; background-color: #252525;">
-        
+    <body id="body" body style="border: 10px solid #252525; color: #FFFFFF; background-color: #252525;">
+    
         <div class="Row">
         
             <div id="time" class="Column">
@@ -88,145 +92,25 @@ include 'config/config.php';
                 <img src="images/log-icon.png" height="125px" />
             </div>
                         
-            <div class="faq">
-                    
-                <input type="search" value="" placeholder="search" />
-                    
-                <ul>
-                        <li id="faq-1">
-                                
-                            <p>
-                            Search Function is NOT working.
-                            </p>
-                                
-                        </li>
-                </ul>  
-                    
-                    <div class="faq__notfound">
-                    <p><strong>No matches were found.</strong></p>
-                    </div>
-                    
+            <div id="search"  class="Column">
+                <input name="text-search" id="text-search" type="text" size="20" maxlength="30" placeholder="search & highlight">
+                <input name="searchit" type="button" value="Search" onClick="highlight()">
             </div>
         
         </div>
-
 
         <?php foreach ($logs as $k => $v) { ?>
             <div class="w3-container w3-center">
                 <h3><span class="w3-text-indigo"><strong><?php echo $k; ?>:</strong></span></h3>
             </div>
                     <div id="slide">
-                        <div class="<?php echo $k; ?>" id="slide-body" style="background-color: #404040; word-wrap: break-word; width: auto; height: 200px; overflow-y: scroll;">
+                        <div class="<?php echo $k; ?>" id="slide-body">
                         <p><?php readExternalLog($v); ?></p>
                         </div>
                         <div id="more">more...</div>
                     </div>
         <?php } ?>
-
-
-        <script>
-
-            'use strict';
-
-            ;( function ( document, window, index )
-            {
-                var hasElementClass     = function( element, className ){ return element.classList ? element.classList.contains( className ) : new RegExp( '(^| )' + className + '( |$)', 'gi' ).test( element.className ); },
-                    addElementClass     = function( element, className ){ element.classList ? element.classList.add( className ) : element.className += ' ' + className; },
-                    removeElementClass  = function( element, className ){ element.classList ? element.classList.remove( className ) : element.className = element.className.replace( new RegExp( '(^|\\b)' + className.split( ' ' ).join( '|' ) + '(\\b|$)', 'gi' ), ' ' ); };
-
-
-                // search & highlight
-
-                ;( function ( document, window, index )
-                {
-                    var container = document.querySelector( '.faq' );
-                    if( !container ) return true;
-
-                    var input           = container.querySelector( 'input' ),
-                        notfound        = container.querySelector( '.faq__notfound' ),
-                        items           = document.querySelectorAll( '.faq > ul > li' ),
-                        item            = {},
-                        itemsIndexed    = [];
-
-                    [].forEach.call( items, function( entry )
-                    {
-                        itemsIndexed.push( entry.textContent.replace( /\s{2,}/g, ' ' ).toLowerCase() );
-                    });
-
-                    input.addEventListener( 'keyup', function( e )
-                    {
-                        if( e.keyCode == 13 ) // enter
-                        {
-                            input.blur();
-                            return true;
-                        }
-
-                        [].forEach.call( items, function( entry )
-                        {
-                            entry.innerHTML = entry.innerHTML.replace( /<span class="highlight">([^<]+)<\/span>/gi, '$1' );
-                        });
-
-                        var searchVal = input.value.trim().toLowerCase();
-                        if( searchVal.length )
-                        {
-                            itemsIndexed.forEach( function( entry, i )
-                            {
-                                if( itemsIndexed[ i ].indexOf( searchVal ) != -1 )
-                                {
-                                    removeElementClass( items[ i ], 'is-hidden' );
-                                    items[ i ].innerHTML = items[ i ].innerHTML.replace( new RegExp( searchVal+'(?!([^<]+)?>)', 'gi' ), '<span class="highlight">$&</span>' );
-                                }
-                                else
-                                    addElementClass( items[ i ], 'is-hidden' );
-                            });
-                        }
-                        else [].forEach.call( items, function( entry ){ removeElementClass( entry, 'is-hidden' ); });
-
-                        if( items.length == [].filter.call( items, function( entry ){ return hasElementClass( entry, 'is-hidden' ) } ).length )
-                            addElementClass( notfound, 'is-visible' );
-                        else
-                            removeElementClass( notfound, 'is-visible' );
-                            
-                    });
-                }( document, window, 0 ));
-
-
-                // auto-show item content when show results reduces to single
-
-                ;( function ( document, window, index )
-                {
-                    var container = document.querySelector( '.faq' );
-                    if( !container ) return true;
-
-                    var input   = container.querySelector( 'input' ),
-                        items   = document.querySelectorAll( '.faq > ul > li' ),
-                        item    = {};
-
-                    input.addEventListener( 'keyup', function( e )
-                    {
-                        item = [].filter.call( items, function( entry ){ return !hasElementClass( entry, 'is-hidden' ); } )
-
-                        if( item.length == 1 )
-                        {
-                            addElementClass( item[ 0 ], 'js--autoshown' );
-                            addElementClass( item[ 0 ], 'is-active' );
-                        }
-                        else
-                            [].forEach.call( items, function( entry )
-                            {
-                                if( hasElementClass( entry, 'js--autoshown' ) )
-                                {
-                                    removeElementClass( entry, 'js--autoshown' );
-                                    removeElementClass( entry, 'is-active' );
-                                }
-                            });
-                    });
-                }( document, window, 0 ));
-
-            }( document, window, 0 ));
-
-        </script>
-
+   
     </body>
     
 </html>
