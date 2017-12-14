9de4c92f9e946a3251880ed220214cc932cdc3a1
Sean Vreeland <seanvree@hotmail.com>
2017-12-14 05:40:16 -0800

    Search re-write
    - Fix:  Search Indicator
    - NEW: Highlight function
    - NEW: Search term indicator

--- assets/js/jquery_search.js
+++ assets/js/jquery_search.js
@@ -1,4 +1,5 @@
 function blockUI() {
+    // e.preventDefault();
     $.blockUI({
         css: {
             'border': 'none',
@@ -8,29 +9,56 @@ function blockUI() {
             '-moz-border-radius': '10px',
             'opacity': '0.8',
             'color': '#EEEBEC',
-        }
+            'font-size': '32px',
+        },
+        message: 'Searching...',
     });
-    message: "Searching...";
-    setTimeout($.unblockUI, 4000);
-}
-
-
-$('document').ready(function() {
-
-    $('#button').click(function() {
-        var searchbox = document.querySelector('#text-search');
-        var searchForm = document.querySelector('#search');
-    });
-});
+    highlight().then(function () {
+        count();
+        $.unblockUI()
+    })
+    setTimeout($.unblockUI, 5000);
+};
 
 
 function highlight() {
+    var def = $.Deferred();
     var text = document.getElementById("text-search").value;
     var query = new RegExp("(\\b" + text + "\\b)", "gim");
-    var e = document.getElementById("body").innerHTML;
-    var enew = e.replace(/(<span>|<\/span>)/igm, "");
-    document.getElementById("body").innerHTML = enew;
-    var newe = enew.replace(query, "<span>$1</span>");
-    document.getElementById("body").innerHTML = newe;
-    color = "#f6f";
-}
+    $("#body span").replaceWith(function (i, e) {
+        return e.textContent
+    });
+    var texts = $("#body, #body *").contents().filter(function () {
+        return this.nodeType === Node.TEXT_NODE;
+    }).get();
+    fix(texts)
+    return def
+
+    function html(s) {
+        return document.createRange().createContextualFragment(s);
+    }
+
+    function fix(elements) {
+        if (elements.length > 0) {
+            var e = elements[0]
+            var t = html(e.textContent.replace(query, "<span>$1</span>"));
+            // console.log(e, t)
+            e.parentNode.insertBefore(t, e);
+            e.parentNode.removeChild(e);
+
+            setTimeout(function () {
+                fix(elements.slice(1))
+            })
+        } else {
+            def.resolve()
+        }
+    }
+};
+
+
+function count() {
+    var count =
+        $("#body span").length;
+        $(".count").text(count);
+        $('#count').append(" occurance(s) of searched term");
+    };
