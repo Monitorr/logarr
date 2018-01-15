fcc07a6243145ba2729d5fa915dfd0fa42ccc14e
Sean Vreeland <seanvree@hotmail.com>
2018-01-15 00:30:21 -0800

    Update logarr.main.js

--- assets/js/logarr.main.js
+++ assets/js/logarr.main.js
@@ -1,7 +1,7 @@
 // Logarr main JS script
 // https://github.com/Monitorr
 
-// Set sytles for BlockUI overlays in /assets/js/jqueryUI.js
+// Set sytles for BlockUI overlays in /assets/js/jquery.blockUI.js
 
 function searchblockUI() {
     $.blockUI({
@@ -21,26 +21,40 @@ function highlight() {
     var enew = e.replace(/(<span>|<\/span>)/igm, "");
     var newe = enew.replace(query, "<span>$1</span>");
     document.getElementById("logcontainer").innerHTML = newe;
-    color = "#f6f";
 };
 
 
 function count() {
+    var text = document.getElementById("text-search").value;
     var count =
-        $("#logcontainer span").length;
-    $(".count").text(count).append(" occurance(s) of searched term");
+        $("#expand span").length;
+        $(".count").text(count);
+        $('.count').append(" occurance(s) of: '");
+        $('.count').append( text );
+        $('.count').append("'");
 };
 
 
 function refreshblockUI() {
-    $.growlUI ('Updating Logs...');
+    $.growlUI('Updating Logs...');
     setTimeout(function () {
-        refresh()
+        refresh();
+        // highlighterror()
     }, 300);
 };
 
 
 function refresh() {
     var url = 'index.php';
-    $('#logwrapper').load(url + ' #logs');
+    $('#logcontainer').load(url + ' #logcontainer');
+};
+
+
+function highlighterror() {
+    text = 'error';
+    var query = new RegExp("(\\b" + text + "\\b)", "gim");
+    var e = document.getElementById("logcontainer").innerHTML;
+    var enew = e.replace(/(<span>|<\/span>)/igm, "");
+    var newe = enew.replace(query, "<span>$1</span>");
+    document.getElementById("logcontainer").innerHTML = newe;
 };
