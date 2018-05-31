b3f0b50f36451b8631bc23e786ccea76dd3db37d
Sean Vreeland <seanvree@hotmail.com>
2018-05-31 08:16:25 -0700

    ADD: Security check for log exists in config file

--- assets/php/unlink.php
+++ assets/php/unlink.php
@@ -4,6 +4,8 @@
         ini_set('display_startup_errors', 1);
         error_reporting(E_ALL);
 
+        include ('../config/config.php');
+
         print_r('Form submitted:  unlink file: ');
         var_dump($_POST['file']);
             echo "<br>";
@@ -19,108 +21,121 @@
 
     echo "<br><br>";
 
+        // check if log file exists in config.php:
+
+    if(in_array($file, $logs)){ 
+
         // check if log file exists:
 
-    if(is_file($file)){
+        if(is_file($file)){
 
-            // copy log file:
+                // copy log file:
 
-        $newfile = "$file.bak";
+            $newfile = "$file.bak";
 
-        if (!copy($file, $newfile)) {
+            if (!copy($file, $newfile)) {
 
-                // copy log file failed:
+                    // copy log file failed:
 
-            echo "Copy log file: FAIL: $newfile";
+                echo "Copy log file: FAIL: $newfile";
 
-            $fh = fopen($file, 'a');
-            fwrite($fh, "$today | ERROR: Logarr was unable to copy log file:  $file\n");
-            
-            fclose($fh);
-
-        }
-
-        else {
-
-                // copy log file success:
-
-            echo "Copy log file: success: $newfile";
-                echo "<br>";
-
-            sleep(2);
-
-                // delete orginal log file:
+                $fh = fopen($file, 'a');
+                fwrite($fh, "$today | ERROR: Logarr was unable to copy log file:  $file\n");
                 
-            $delete = unlink($file);
-
-            sleep(2);
-
-
-            if($delete == true) {
-
-                echo "Delete original log file: success: $file\n"; 
-
-                    echo "<br>"; 
-
-                $newlogfile = $file;
-
-                    // Write log entry in new log file:
-
-                $current = $today . " | Logarr created new log file: " . $newlogfile . "\n";
-                
-                $createfile = file_put_contents($newlogfile, $current);
-
-                if($createfile == true) {
-                
-                    echo "Create new log file: success: " . $newlogfile; 
-
-                        echo "<br>"; 
-
-                }
-
-                else {
-                
-                    echo "Create new log file: FAIL: " . $newlogfile;
-
-                        echo "<br>"; 
-                }
+                fclose($fh);
 
             }
 
             else {
-                echo "Delete original log file: FAIL: $file\n";
 
-                    echo "<br>"; 
+                    // copy log file success:
 
-                    //write log file entry if unlink of original log file fails:
+                echo "Copy log file: success: $newfile";
+                    echo "<br>";
 
-                $fh = fopen($file, 'a');
-                fwrite($fh, "$today | Logarr delete original log file: FAIL (ERROR):  $file\n");
-                fclose($fh);
+                sleep(2);
 
-                    //remove copied log file if unlink of original log file fails: 
-                
-                $deletefail = unlink($newfile);
+                    // delete orginal log file:
+                    
+                $delete = unlink($file);
 
-                if($deletefail == true) {
+                sleep(2);
 
-                    echo "Delete log file backup: Success: $newfile";
+
+                if($delete == true) {
+
+                    echo "Delete original log file: success: $file\n"; 
+
+                        echo "<br>"; 
+
+                    $newlogfile = $file;
+
+                        // Write log entry in new log file:
+
+                    $current = $today . " | Logarr created new log file: " . $newlogfile . "\n";
+                    
+                    $createfile = file_put_contents($newlogfile, $current);
+
+                    if($createfile == true) {
+                    
+                        echo "Create new log file: success: " . $newlogfile; 
+
+                            echo "<br>"; 
+
+                    }
+
+                    else {
+                    
+                        echo "Create new log file: FAIL: " . $newlogfile;
+
+                            echo "<br>"; 
+                    }
 
                 }
 
                 else {
+                    echo "Delete original log file: FAIL: $file\n";
 
-                    echo "Delete log file backup: FAIL: $newfile";
-                }
+                        echo "<br>"; 
+
+                        //write log file entry if unlink of original log file fails:
+
+                    $fh = fopen($file, 'a');
+                    fwrite($fh, "$today | Logarr delete original log file: FAIL (ERROR):  $file\n");
+                    fclose($fh);
+
+                        //remove copied log file if unlink of original log file fails: 
                     
+                    $deletefail = unlink($newfile);
+
+                    if($deletefail == true) {
+
+                        echo "Delete log file backup: Success: $newfile";
+
+                    }
+
+                    else {
+
+                        echo "Delete log file backup: FAIL: $newfile";
+                    }
+                        
+                }
             }
         }
-    }
+
+        else {
+
+            echo 'file: ' . $file . ' does not exist.';
+        
+        }
+
+    } 
+    
+        // Deny access if log file does NOT exist in config.php:
 
     else {
-
-        echo 'file: ' . $file . ' does not exist.';
-    
+        echo 'ERROR:  Illegal File';
     }
+
 
 ?>
