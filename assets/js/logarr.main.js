099e461fbf67914e70d284cec9c76b439a02b089
Sean Vreeland <seanvree@hotmail.com>
2018-06-03 08:17:25 -0700

    FIX: jump and scroll!  Dope.

--- assets/js/logarr.main.js
+++ assets/js/logarr.main.js
@@ -62,18 +62,26 @@ $(function () {
 
         //Jumps to the element matching the currentIndex
      
-    function jumpTo() {
-        if ($results.length) {
-            var position,
-                $current = $results.eq(currentIndex);
-            $results.removeClass(currentClass);
-            if ($current.length) {
-                $current.addClass(currentClass);
-                position = $current.offset().top - offsetTop;
-                window.scrollTo(0, position);
+        function jumpTo() {
+            if ($results.length) {
+                var position,
+                    $current = $results.eq(currentIndex);
+                $results.removeClass(currentClass);
+                if ($current.length) {
+                    $current.addClass(currentClass);
+                    var currentOffset = $('.markresults.current').offsetTop;
+                    var parent = $('.markresults.current').parent();
+                    while (!parent.is('div')) {
+                        parent = parent.parent();
+                    }
+
+                    /* not animated scroll */
+                    parent.scrollTop(
+                        $('.markresults.current').offset().top - parent.offset().top + parent.scrollTop()
+                    );
+                }
             }
         }
-    }
 
     function mark() {
 
