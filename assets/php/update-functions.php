7668964f4b29db3b429795cc693ce77bbf5c43ab
Rob Gökemeijer <grob1998@me.com>
2018-06-22 16:00:58 +0200

    Prevent custom.css from being overwritten by an update

--- assets/php/update-functions.php
+++ assets/php/update-functions.php
@@ -24,14 +24,18 @@ if ($copy == 1) {
     if ($res === TRUE) {
         $zip->extractTo($extractPath);
         $zip->close();
-        // copy config.json to safe place while we update
-        rename($datadir . '/config.json', $extractPath . 'config.json');
-        // copy files from temp to monitorr root
+	    // copy config.json to safe place while we update
+	    rename($datadir . '/config.json', $extractPath . 'config.json');
+	    // copy config.json to safe place while we update
+	    rename(__DIR__ . '/../data/custom.css', $extractPath . 'custom.css');
+        // copy files from temp to Logarr root
         $scanPath = array_diff(scandir($extractPath), array('..', '.'));
         $fullPath = $extractPath . $scanPath[2];
         recurse_copy($fullPath, $base_path);
-        // restore config.json file
-        rename($extractPath . 'config.json', $datadir . '/config.json');
+	    // restore config.json file
+	    rename($extractPath . 'config.json', $datadir . '/config.json');
+	    // restore custom.css file
+	    rename($extractPath . 'custom.css', __DIR__ . '/../data/custom.css');
         // update users local version number file
         $userfile = fopen("../js/version/version.txt", "w");
         $user_vnum = fgets($userfile);
