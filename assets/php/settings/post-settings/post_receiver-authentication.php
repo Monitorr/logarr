9029741a733097dccfeb0b35c57f74037d3648ca
Rob Gökemeijer <grob1998@me.com>
2018-06-24 14:55:15 +0200

    Check settings POST data

--- assets/php/settings/post-settings/post_receiver-authentication.php
+++ assets/php/settings/post-settings/post_receiver-authentication.php
@@ -1,10 +1,10 @@
 <?php
+if(isset($_POST) && !empty($_POST)) {
+	include(__DIR__ . '/../../functions.php');
+	$str = file_get_contents($config_file);
 
-include(__DIR__ . '/../../functions.php');
-$str = file_get_contents($config_file);
+	$json = json_decode($str, true);
+	$json['authentication'] = $_POST;
 
-$json = json_decode($str, true);
-$json['authentication'] = $_POST;
-
-file_put_contents($config_file, json_encode($json, JSON_PRETTY_PRINT));
-
+	file_put_contents($config_file, json_encode($json, JSON_PRETTY_PRINT));
+}
