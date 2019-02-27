8707f9d66e2ad75b7093499247c762bbbbecddc3
Rob Gökemeijer <grob1998@me.com>
2019-02-27 14:06:29 +0100

    Remove footer from register

--- assets/php/authentication/register.php
+++ assets/php/authentication/register.php
@@ -208,24 +208,6 @@ include_once(__DIR__ . "/../auth_check.php");
 </div>
 <!--  END create user form -->
 
-<div id="footer">
-
-	<!-- Checks for Logarr application update on page load & "Check for update" click: -->
-	<script src="assets/js/update.js" async></script>
-
-	<div id="logarrid">
-		<a href="https://github.com/monitorr/logarr" title="Logarr GitHub repo" target="_blank"
-		   class="footer">Logarr </a> |
-
-		<a href="https://github.com/Monitorr/logarr/releases" title="Logarr releases" target="_blank" class="footer">
-			Version: <?php echo file_get_contents(__DIR__ . "/../../js/version/version.txt"); ?>
-		</a> |
-		<a href="settings.php" title="Logarr Settings" target="_blank" class="footer">Settings</a>
-		<br>
-	</div>
-
-</div>
-
 </body>
 
 </html>
