a7c3b792bb8dd2692b6dd633854e5be7b18cfa93
Sean Vreeland <seanvree@hotmail.com>
2019-03-06 23:48:31 -0700

    testing in progress - 07 MAR

---
+++ assets/js/login-status.js
@@ -0,0 +1,49 @@
+function checkLogin() {
+
+    //console.log('Logarr auth: ENABLED | Checking login status');
+
+    $.ajax({
+        type: "GET",
+        url: "assets/php/login-status.php",
+        // data: info,
+        success: function (data) {
+
+            if (data === "true") {
+                // User is logged IN:
+
+                console.log('Logger user is logged IN');
+
+            } else {
+                // User is logged OUT:
+
+                console.log('Logger user is logged OUT');
+                logouttoast();
+
+                // If user user logs out, refresh index page to envoke authentication page after 3s:
+                setTimeout(function () {
+
+                    // TODO what works for both index and settings ??
+                    
+                    window.location.href = "index.php"; // THIS WORKS
+
+                    //window.top.location; // DOES NOT WORK
+                    
+                    //window.top.location.reload(); Does not work
+
+                    //top.location.reload();  //Doesn't work on settings page if in iframe
+
+                }, 3000);
+            }
+        },
+
+        error: function () {
+            // error
+            console.log('ERROR: An error occurred while checking login status.');
+        }
+    });
+
+    //check login status every 10s:
+    setTimeout(checkLogin, 10000);
+}
+
+checkLogin();
